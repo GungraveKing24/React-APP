0be79ae5e68980ec97bb730d3747c18123f7ab1b
garciab1 <eg9446930@gmail.com>
2025-03-19 14:48:55 -0600

    Conditions

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Homepage from "./pages/Clients/Homepage.jsx"
 import Navbar from './pages/Both/Navbar.jsx';
 import Footer from './pages/Both/Footer.jsx';
 import Contact from "./pages/Clients/Contact";
+import Policiaes from "./pages/Clients/Policies_conditions.jsx"
+
 
 function App() {
   return (
@@ -21,6 +23,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/policies" element={<Policiaes />} />
       </Routes>
 
       <Footer />
---
+++ src/pages/Clients/Policies_conditions.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+export default function Policies() {
+    return (
+      <div className="bg-white p-10">
+        {/* Título */}
+        <h2 className="text-3xl font-bold text-center mb-6 text-yellow-700">
+          Políticas y Condiciones
+        </h2>
+  
+        {/* Primera sección de texto */}
+        <p className="text-gray-700 text-justify max-w-3xl mx-auto leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum metus vel augue egestas, 
+          sed bibendum magna vestibulum. Donec nec augue at risus molestie convallis. Cras venenatis dui 
+          nec sapien aliquet tincidunt.
+        </p>
+  
+        {/* Separador */}
+        <div className="w-full h-10 bg-yellow-100 mt-8"></div>
+  
+        {/* Sección de imágenes */}
+        <div className="mt-6 grid grid-cols-3 gap-4 max-w-4xl mx-auto">
+          <img src="/images/pic1.png" alt="Flores 1" className="rounded-lg shadow-md" />
+          <img src="/images/pic2.png" alt="Tarjeta" className="rounded-lg shadow-md" />
+          <img src="/images/pic3.png" alt="Flores 2" className="rounded-lg shadow-md" />
+        </div>
+  
+        {/* Segunda sección de texto */}
+        <p className="text-gray-700 text-justify max-w-3xl mx-auto mt-8 leading-relaxed">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+          totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        </p>
+  
+        {/* Footer */}
+      <footer className="bg-[#D4A5A5] py-8 px-6 text-center text-white">
+        <p className="text-lg font-semibold">Somos una floristería en línea</p>
+        <p className="text-sm mt-2">Siguenos por medio de Instagram</p>
+      </footer>
+    </div>
+    );
+  }
+  
