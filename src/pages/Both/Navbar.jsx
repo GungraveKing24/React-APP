ca326c15f08d76c28d43754a38b266ef8789dfd2
Gungrave <jorgefranciscocz@gmail.com>
2025-04-19 17:20:38 -0600

    Search integration

--- src/pages/Both/Navbar.jsx
+++ src/pages/Both/Navbar.jsx
@@ -95,30 +95,6 @@ export default function Navbar() {
             </li>
           </ul>
 
-          {/* Barra de búsqueda */}
-          <div className="relative w-full max-w-md mx-auto">
-            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-              <svg 
-                className="w-5 h-5 text-gray-400" 
-                viewBox="0 0 24 24" 
-                fill="none"
-              >
-                <path 
-                  d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" 
-                  stroke="currentColor" 
-                  strokeWidth="2" 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </span>
-            <input
-              type="text"
-              className="w-full py-2 pl-10 pr-4 font-Title text-gray-700 bg-white border border-gray-300 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
-              placeholder="Buscar..."
-            />
-          </div>
-
           {/* Carrito de compras */}
           <div className="relative flex justify-center md:justify-start items-center">
             <Link 
